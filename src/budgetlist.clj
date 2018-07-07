99ff94afa2f9484efe8213256895df6a56b099df
Chonlasith Jucksriporn <chonlasith@gmail.com>
2018-07-07 13:46:41 +0700

    apply thready macro

--- src/budgetlist.clj
+++ src/budgetlist.clj
@@ -12,19 +12,24 @@
         below-end? (fn [t] (t/within? (t/interval start end) t))]
     (take-while below-end? inf-range)))
 
-(defn number-of-days-in-the-month
-  [dt]
-  (t/day (t/last-day-of-the-month- dt)))
+(defn number-of-days-in-the-month [dt]
+  (->> dt
+       (t/last-day-of-the-month-)
+       (t/day)))
 
-(defn budget-by-day
-  [day]
+(defn budget-by-day [day]
   (let [days (number-of-days-in-the-month day)]
-    (/ (get budget-pack (f/unparse yyyy-mm-formatter day) 0) days)))
+    (as-> day d
+      (f/unparse yyyy-mm-formatter d)
+      (get budget-pack d 0)
+      (/ d days))))
 
 (defn find-budget-between
   [from to]
   (let [r (time-range from (t/plus to (t/days 1)) (t/days 1))]
-    (reduce + (map budget-by-day r))))
+    (->> r
+         (map budget-by-day)
+         (reduce +))))
 
 (defn -main []
   (def from (t/date-time 2018 2 1))
